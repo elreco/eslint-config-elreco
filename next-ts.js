006f2f2f00cb410668922c56e5a44a4871d27c97
Alexandre Le Corre <a.elreco@gmail.com>
2024-03-23 15:40:45 +0100

    add prettier plugin tailwind

--- next-ts.js
+++ next-ts.js
@@ -1,45 +1,46 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'next',
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next",
   ],
   root: true,
-  plugins: ['@typescript-eslint'],
+  plugins: ["@typescript-eslint", 'prettier-plugin-tailwindcss'],
   rules: {
-    '@next/next/no-img-element': 'off',
-    '@next/next/no-page-custom-font': 'off',
-    'react/display-name': 'off',
-    'react/no-children-prop': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'prettier/prettier': [
+    "@next/next/no-img-element": "off",
+    "@next/next/no-page-custom-font": "off",
+    "react/display-name": "off",
+    "react/no-children-prop": "off",
+    "react/react-in-jsx-scope": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "prettier/prettier": [
       "error",
       {
-        "endOfLine": "auto"
-      }
+        endOfLine: "auto",
+        plugins: ["prettier-plugin-tailwindcss"],
+      },
     ],
-    'import/order': [
+    "import/order": [
       "error",
       {
         "newlines-between": "always",
-        "groups": [
+        groups: [
           "builtin",
           "external",
           "internal",
           "parent",
           "sibling",
-          "index"
+          "index",
         ],
-        "alphabetize": {
-          "order": "asc",
-          "caseInsensitive": true
-        }
-      }
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
     ],
   },
-  ignorePatterns: ['*.js', 'node_modules/'],
+  ignorePatterns: ["*.js", "node_modules/"],
   env: {
     browser: true,
     node: true,
